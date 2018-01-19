9739e84558b34e78eb8150653a723806f8d8667e
onahump <oswaldo@makingdevs.com>
2018-01-18 23:11:28 -0600

    Adding a new variable and some comments

--- hello_world.rs
+++ hello_world.rs
@@ -1,5 +1,13 @@
-//Mi primer Hola mundo
+//Mi primer Hola mundo - comentario de una sola linea
 fn main(){
   println!("Ya estoy aqui perros!");
+/* this
+  is
+  a
+  multiline
+  commentary
+*/
+  let x = 5 /*Adding new commentary*/ + 5;
   println!("Hi, bitches i'm here!");
+  println!("What's the number x? x = {}",x );
 }
